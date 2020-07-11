aa4271f3e01707e03b6fe6e4049e7508395d0898
johnnylongbeach <johnnylongbeach@outlook.com>
2020-07-11 00:21:58 -0500

    update helloworld to ask questions

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,9 @@
 print("Hello World")
+print("What is your name?") #ask for name
+myName = input()
+print("It's good to meet you, " + myName)
+print("Length of your name is:")
+print(len(myName))
+print("What is your age?") #ask for age
+myAge = input()
+print("You will be" + str(int(myAge) + 1)+ "in a year")
